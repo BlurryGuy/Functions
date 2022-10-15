cb4bf8e8e2834cbac26923109c6f60043ced74f7
Blurry <114656831+BlurryGuy@users.noreply.github.com>
2022-10-15 12:41:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def myfunction(text): # Naming the function
     print(text) # Printing whatever the first argument is being set to with the myfunction Function, must have a indentation block
+    input() # Stopping the console from closing
 
 myfunction("Hello.") # Using the myfunction Function
