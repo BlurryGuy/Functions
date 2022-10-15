4b6c9bd68af1888b93ee4504e6758e3cf3907952
Blurry <114656831+BlurryGuy@users.noreply.github.com>
2022-10-15 12:40:33 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+def myfunction(text): # Naming the function
+    print(text) # Printing whatever the first argument is being set to with the myfunction Function, must have a indentation block
+
+myfunction("Hello.") # Using the myfunction Function
